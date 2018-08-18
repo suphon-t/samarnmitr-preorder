1d0dc3aa05ff8c8413501e8a41377b3df64fe92f
paphonb <paphonb@gmail.com>
2018-08-18 20:54:36 +0700

    Show admin name in manage home

--- resources/assets/js/modules/Manage/scenes/Home.js
+++ resources/assets/js/modules/Manage/scenes/Home.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class ManageHome extends Component {
+class ManageHome extends Component {
 
     render() {
         return (
-            <h1>Hello!</h1>
+            <h1>Hello { this.props.user.name }!</h1>
         )
     }
 }
+
+const mapStateToProps = state => ({
+    user: state.user
+})
+
+export default connect(mapStateToProps)(ManageHome)
