36ddb82e3b2b89e76148a35cfc25b31c4ff492ea
paphonb <paphonb@gmail.com>
2018-08-19 23:16:51 +0700

    Implement set image in desktop home

--- resources/assets/js/modules/Shop/components/Sets/Set.js
+++ resources/assets/js/modules/Shop/components/Sets/Set.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
 import routes from '../../../../routes/routes'
+import { getImage } from '../../shopUtils'
 
 export default props => {
     const { set } = props
     return (
         <Link className="set-card" to={routes.shop.detail.get({ productId: set.id })}>
-            <div className="set-pic" />
+            <div className="set-pic">
+                <img className="fit-parent" src={getImage(set.id)} />
+            </div>
             <div className="set-text">
                 { set.name }
             </div>
